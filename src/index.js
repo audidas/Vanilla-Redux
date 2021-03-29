9abcc6eda5c3b66c3b703118730bb2d3c1870914
audidas <audidas@naver.com>
2021-03-29 14:55:05 +0900

    순수 자바스크립트 리덕스 끝

--- src/index.js
+++ src/index.js
@@ -1,24 +1,63 @@
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
+import { createStore } from "redux";
 
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-let count = 0;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const addToDo = (text) => {
+  return { type: ADD_TODO, text };
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
+const deleteToDo = (id) => {
+  return { type: DELETE_TODO, id };
 };
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return state.filter((toDo) => toDo.id !== action.id);
+    default:
+      return state;
+  }
 };
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const store = createStore(reducer);
+
+const dispatchAddTodo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddTodo(toDo);
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "삭제";
+    btn.addEventListener("click", dispatchDeleteTodo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
+
+form.addEventListener("submit", onSubmit);
